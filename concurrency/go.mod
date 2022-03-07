b39c591bcf82076c2e6ebb377a38c0b485f01789
Prathy R <keerthi@Keerthis-MBP.lan>
2022-03-06 23:56:05 -0800

    concurrency: simple pool

--- concurrency/go.mod
+++ concurrency/go.mod
@@ -1,3 +1,8 @@
 module github.com/stackpath/backend-developer-tests/concurrency
 
 go 1.15
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/sirupsen/logrus v1.8.1
+)
--- concurrency/simple_pool.go
+++ concurrency/simple_pool.go
@@ -2,6 +2,15 @@
 // bit more complex.
 package concurrency
 
+import (
+	"sync"
+
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+)
+
+var log = logrus.New()
+
 // SimplePool is a simple worker pool that does not support cancellation or
 // closing. All functions are safe to call from multiple goroutines.
 type SimplePool interface {
@@ -11,9 +20,58 @@ type SimplePool interface {
 	Submit(func())
 }
 
+type WorkerPool struct {
+	numberOfWorkers int
+	jobs            chan Job
+	wg              sync.WaitGroup
+}
+
+type Job struct {
+	id       uuid.UUID
+	function func()
+}
+
 // NewSimplePool creates a new SimplePool that only allows the given maximum
 // concurrent tasks to run at any one time. maxConcurrent must be greater than
 // zero.
 func NewSimplePool(maxConcurrent int) SimplePool {
-	panic("TODO")
+	jobsChan := make(chan Job, maxConcurrent)
+	sp := WorkerPool{numberOfWorkers: maxConcurrent, jobs: jobsChan}
+	sp.spawnWorkers()
+	return &sp
+}
+
+// Submit adds new task to the job queue
+func (wp *WorkerPool) Submit(fn func()) {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		return
+	}
+	job := Job{id: id, function: fn}
+	wp.jobs <- job
+}
+
+// spawnWorkers spwans new woker
+func (wp *WorkerPool) spawnWorkers() {
+	for i := 0; i < wp.numberOfWorkers; i++ {
+		log.Infof("%d workers in the pool", wp.numberOfWorkers-i+1)
+		wp.wg.Add(1)
+		go wp.work()
+	}
+	// wait till all workers are done processing
+	wp.wg.Wait()
+}
+
+// work gets task in the queue
+// and finishes processing the job
+func (wp *WorkerPool) work() {
+	for job := range wp.jobs {
+		log.Infof("Dispatching job %v", job.id)
+		wp.executeFunction(job.function)
+	}
+	defer wp.wg.Done()
+}
+
+func (wp *WorkerPool) executeFunction(fn func()) {
+	// execute the function in args
 }
